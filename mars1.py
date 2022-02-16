62d40cfc6958084ac34540372180759691423983
d1larafaizullina <94979657+d1larafaizullina@users.noreply.github.com>
2022-02-16 16:57:30 +0500

    mars1 Результаты отбора

--- mars1.py
+++ mars1.py
@@ -134,6 +134,30 @@ def choice(planet_name):
                 </html>'''
 
 
+# Результат отбора
+@app.route('/results/<nickname>/<int:level>/<float:rating>')
+def results(nickname, level, rating):
+    return f'''<!doctype html>
+                <html lang="en">
+                  <head>
+                    <meta charset="utf-8">
+                    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+                    <link rel="stylesheet"
+                   href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
+                   integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
+                   crossorigin="anonymous">
+                    <title>Результаты отбора</title>
+                  </head>
+                  <body>
+                    <h1>Результаты отбора</h1>
+                    <h3>Претендента на участие в миссии {nickname}:</h3>
+                    <div class="alert alert-success">Поздравляем! Ваш рейтинг после {level} этапа отбора</div>
+                    <div class="alert alert-secondary">составляет {rating}!</div>
+                    <div class="alert alert-warning">Желаем удачи!</div>
+                  </body>
+                </html>'''
+
+
 if __name__ == '__main__':
     print(os.getcwd())
     app.run(port=8080, host='127.0.0.1', debug=True)
